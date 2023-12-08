fix: PNE calculation

main.py
@@ -13,9 +13,9 @@ from utils.delta import calculate_delta
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-N", type=int, default=4)
-    parser.add_argument("-K", type=int, default=4)
-    parser.add_argument("-T", type=int, default=1000000)
+    parser.add_argument("-N", type=int, default=10)
+    parser.add_argument("-K", type=int, default=2)
+    parser.add_argument("-T", type=int, default=3000000)
     parser.add_argument(
         "--dis", type=str, choices=["bernoulli", "beta"], default="beta"
     )
@@ -44,11 +44,11 @@ def parse_args():
     parser.add_argument("--tolerance", type=float, default=1e-6)
 
     # Ours
-    parser.add_argument("--c1", type=float, default=0.01)
-    parser.add_argument("--c2", type=float, default=1000)
+    parser.add_argument("--c1", type=float, default=0.0001)
+    parser.add_argument("--c2", type=float, default=100000000)
     parser.add_argument("--c3", type=float, default=100)
     parser.add_argument("--eta", type=float, default=0)
-    parser.add_argument("--epsilon", type=float, default=5e-3)
+    parser.add_argument("--epsilon", type=float, default=1e-2)
     parser.add_argument("--debug", action="store_true", default=False)
     parser.add_argument("--no-gamma", action="store_true", default=False)
 
@@ -107,8 +107,12 @@ def main():
     total_runs = 50
     pne_nums = []
     regrets_sum = []
+    if args.debug:
+        last_mood = ["discontent"] * args.N
+        last_choice = [0] * args.N
+        last_utility = [0] * args.N
     for seed_data in range(0, total_runs):
-        if args.debug and seed_data != 0:
+        if args.debug and seed_data != 1:
             continue
 
         print("Running {}/{}".format(seed_data + 1, total_runs))
@@ -116,13 +120,18 @@ def main():
         res_file = os.path.join(res_path_base, "{}.pkl".format(seed_data))
         count_tmp = np.zeros(8)
 
-        if not os.path.exists(res_file) or args.method == "Ours":
-            loop = Loop(N, K, T, dis=dis, cate=cate, seed=seed_data)
+        if (not os.path.exists(res_file)) or args.debug:
+            if args.debug:
+                loop = Loop(N, K, T, dis=dis, cate=cate, seed=seed_data)
+            else:
+                loop = Loop(N, K, T, dis=dis, cate=cate, seed=seed_data)
             print(loop.mu)
             print(loop.delta)
             print(loop.weights)
             if args.debug:
-                print(calculate_delta(loop.weights, loop.mu, isprint=True))
+                res_delta = calculate_delta(loop.weights, loop.mu, isprint=True)
+                print(res_delta)
+                exit()
             players = []
             for i in range(args.N):
                 if method == "TotalReward":
@@ -179,44 +188,51 @@ def main():
                 for i in range(N):
                     players[i].update(arm_rewards[i], personal_rewards[i], choices)
 
-                if args.debug:
-                    print(
-                        "out:",
-                        choices,
-                        regrets,
-                        is_pne,
-                        loop.tmp_personal_expected_rewards,
-                        loop.welfare,
-                    )
                 if args.method == "Ours" and args.debug:
-                    choices_new = []
-                    for i in range(N):
-                        choices_new.append(np.argmax(players[0].count_best))
-                    _, _, is_pne_new, regrets_new = loop.pull(choices_new, t)
-                    # tmp = choices[0] * 4 + choices[1] * 2 + choices[2]
-                    # if players[0].mood == "content" and players[1].mood == "content" and players[2].mood == "content":
-                    #     count_tmp[tmp] += 1
-                    print(
-                        [
-                            (
-                                player.mood,
-                                player.action,
-                                round(player.utility, 2),
-                                round(player.last_utility, 2),
-                            )
-                            for player in players
-                        ],
-                        choices,
-                        is_pne,
-                        loop.mu.round(2),
-                    )
-                    print(
-                        "counting best: ",
-                        [player.count_best.argmax() for player in players],
-                    )
+                    flag = False
+                    for i in range(args.N):
+                        if players[i].mood != last_mood[i] or players[i].action != last_choice[i]:
+                            flag = True
+                            break
+                    if flag:
+                        print("=" * 15 + "t={}".format(t) + "=" * 15)
+                        print(choices)
+                        print(
+                            [
+                                (
+                                    last_mood[i][0],
+                                    last_choice[i],
+                                    round(last_utility[i], 6),
+                                )
+                                for i in range(args.N)
+                            ],
+                        )
+                        print(
+                            [
+                                (
+                                    player.mood[0],
+                                    player.action,
+                                    round(player.utility, 6),
+                                )
+                                for player in players
+                            ],
+                        )
+                        print([player.true_utility.round(6) for player in players])
+                        print( is_pne,
+                            regrets.round(2).tolist(),
+                            loop.mu.round(2))
+                        print(
+                            "counting best: ",
+                            [player.count_best.argmax() for player in players],
+                        )
+                        print("delta:", res_delta)
                     # if choices == [3, 0, 1, 2]:
                     #     exit()
                     # exit()
+
+                    last_mood = [player.mood for player in players]
+                    last_choice = [player.action for player in players]
+                    last_utility = [player.utility for player in players]
 
             res_arm_rewards = np.concatenate(res_arm_rewards, axis=0)
 

model/data.py
@@ -125,7 +125,7 @@ class Loop:
         for i, choice in enumerate(choices):
             weight[i][choice] -= self.weights[i][choice]
 
-        weight_choices = np.tile(weight_choices.reshape(self.N, 1), [1, self.K])
+        weight_choices = self.weights
         weight = weight + weight_choices
         reward_deviation = (
             np.tile(self.mu.reshape(-1, self.K), [self.N, 1])
@@ -136,8 +136,8 @@ class Loop:
         regrets = np.maximum(0, reward_best_deviation - personal_expected_rewards)
 
         regrets = np.array(regrets)
-        is_pne = (np.sum(regrets) <= 1e-6) and (
-            np.sum(personal_expected_rewards) >= self.welfare - 1e-6
+        is_pne = (np.sum(regrets) <= 1e-4) and (
+            np.sum(personal_expected_rewards) >= self.welfare - 1e-4
         )
         self.tmp_personal_expected_rewards = np.sum(personal_expected_rewards)
         return arm_rewards, personal_rewards, is_pne, regrets

model/method.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from utils.utils import set_seed
+from utils.utils import set_seed, DENOMINATOR_DELTA, THRESHOLD
 
 
 def calculate_PNE(mu, N):
@@ -269,7 +269,7 @@ class Ours:
         self.mood = "discontent"
         self.action = 0
         self.utility = 0
-        self.last_utility = 0
+        self.true_utility = 0
 
         self.remaining = 0
         self.round = 0
@@ -277,7 +277,6 @@ class Ours:
 
         self.count_best = np.zeros(self.K)
 
-        self.threshold = 1e-5
 
         self.log_probabilities = np.log(np.random.rand(self.T // 2))
         self.prob_idx = 0
@@ -302,7 +301,7 @@ class Ours:
             k = (t + self.rank) % self.K
         else:
             if t == self.c1 * self.K:
-                self.mu_hat = self.rewards / (self.count + 1e-6)
+                self.mu_hat = self.rewards / (self.count + DENOMINATOR_DELTA)
                 self.phase = "exploiting"
                 if self.debug:
                     self.mu_hat = self.loop.mu
@@ -312,7 +311,7 @@ class Ours:
                     self.round += 1
                     self.phase = "learning"
                     self.remaining = np.ceil(self.c2 * np.power(self.round, self.eta))
-                    self.mu_hat = self.rewards / (self.count + 1e-6)
+                    self.mu_hat = self.rewards / (self.count + DENOMINATOR_DELTA)
                     if self.debug:
                         self.mu_hat = self.loop.mu
                 else:
@@ -326,12 +325,12 @@ class Ours:
                     self.utility = (
                         self.mu_hat[k]
                         * self.last_personal_reward
-                        / (self.last_arm_reward + 1e-6)
+                        / (self.last_arm_reward + DENOMINATOR_DELTA)
                         + self.gammas[k]
                     )
             else:
                 if self.mood == "discontent":
-                    k = np.random.randint(0, self.K)
+                    k = np.random.choice(self.K, p=self.mu_hat / np.sum(self.mu_hat))
                 elif self.mood == "content":
                     log_p = self.next_log_prob()
                     if log_p >= self.log_epsilon:
@@ -359,9 +358,9 @@ class Ours:
                 utility = 0
             else:
                 utility = (
-                    personal_reward / (arm_reward) * self.mu_hat[k] + self.gammas[k]
+                    personal_reward / (arm_reward + DENOMINATOR_DELTA) * self.mu_hat[k] + self.gammas[k]
                 )
-            self.last_utility = utility
+            self.true_utility = utility
             log_p = self.next_log_prob()
             if self.mood == "discontent":
                 if log_p <= self.F(utility) * self.log_epsilon:
@@ -370,33 +369,33 @@ class Ours:
                     self.utility = utility
             elif self.mood == "content":
                 if k == self.action:
-                    if utility > self.utility + self.threshold:
+                    if utility > self.utility + THRESHOLD:
                         self.mood = "hopeful"
-                    elif np.abs(utility - self.utility) < self.threshold:
+                    elif np.abs(utility - self.utility) < THRESHOLD:
                         self.mood = "content"
                     else:
                         self.mood = "watchful"
                 else:
                     log_p = self.next_log_prob()
                     if (
-                        utility >= self.utility + self.threshold
+                        utility >= self.utility + THRESHOLD
                         and log_p <= self.G(utility - self.utility) * self.log_epsilon
                     ):
                         self.mood = "content"
                         self.action = k
                         self.utlity = utility
             elif self.mood == "watchful":
-                if utility > self.utility + self.threshold:
+                if utility > self.utility + THRESHOLD:
                     self.mood = "hopeful"
-                elif np.abs(utility - self.utility) < self.threshold:
+                elif np.abs(utility - self.utility) < THRESHOLD:
                     self.mood = "content"
                 else:
                     self.mood = "discontent"
             elif self.mood == "hopeful":
-                if utility > self.utility + self.threshold:
+                if utility > self.utility + THRESHOLD:
                     self.mood = "content"
                     self.utility = utility
-                elif np.abs(utility - self.utility) < self.threshold:
+                elif np.abs(utility - self.utility) < THRESHOLD:
                     self.mood = "content"
                 else:
                     self.mood = "watchful"

utils/delta.py
@@ -5,6 +5,7 @@ import numpy as np
 from tqdm import tqdm
 
 from model.method import calculate_PNE
+from utils.utils import DENOMINATOR_DELTA, THRESHOLD
 
 
 def calculate_payoff(weights, rewards, strategy):
@@ -88,7 +89,7 @@ def find_pne_1(weights, rewards, isprint=False):
             total_payoff = sum(current_payoffs)
             if first_payoff == -1:
                 first_payoff = total_payoff
-            elif abs(total_payoff - first_payoff) > 1e-5:
+            elif abs(total_payoff - first_payoff) > THRESHOLD:
                 multiple_pne = True
             if isprint:
                 print("PNE", strategy, total_payoff, delta_1)
@@ -131,25 +132,26 @@ def find_pne_2(weights, rewards, isprint=False):
 
         weight_choices = np.array(weight_choices)
 
-        personal_expected_rewards = (rewards / (weight + 1e-6))[strategy]
+        personal_expected_rewards = (rewards / (weight + DENOMINATOR_DELTA))[strategy]
         personal_expected_rewards = personal_expected_rewards * weight_choices
 
         weight = np.tile(weight, N).reshape(N, -1)
+
         for i, choice in enumerate(strategy):
             weight[i][choice] -= weights[i][choice]
 
-        weight_choices = np.tile(weight_choices.reshape(N, 1), [1, K])
-        weight = weight + weight_choices
+        weight = weight + weights
+
         reward_deviation = (
-            np.tile(rewards.reshape(-1, K), [N, 1]) / (weight + 1e-6) * weight_choices
+            np.tile(rewards.reshape(-1, K), [N, 1]) / (weight + DENOMINATOR_DELTA) * weights
         )
 
         for i, choice in enumerate(strategy):
-            reward_deviation[i][choice] = -1e6
+            reward_deviation[i][choice] = -THRESHOLD
 
         reward_best_deviation = np.max(reward_deviation, axis=1)
         delta_rewards = reward_best_deviation - personal_expected_rewards
-        if np.max(delta_rewards) <= 1e-6:
+        if np.max(delta_rewards) <= THRESHOLD:
             is_pne = True
             delta_1 = -np.max(delta_rewards)
         else:
@@ -163,10 +165,11 @@ def find_pne_2(weights, rewards, isprint=False):
                 best_payoff = total_payoff
             if first_payoff == -1:
                 first_payoff = total_payoff
-            elif abs(total_payoff - first_payoff) > 1e-5:
+            elif abs(total_payoff - first_payoff) > THRESHOLD:
                 multiple_pne = True
             if isprint:
                 print("PNE", strategy, total_payoff, delta_1)
+                print(delta_rewards, reward_best_deviation, personal_expected_rewards, rewards)
             if delta_1 <= best_delta_1:
                 best_pne = strategy
                 best_delta_1 = delta_1
@@ -220,25 +223,25 @@ def calculate_SMAA(weights, rewards, isprint=False):
 
         weight_choices = np.array(weight_choices)
 
-        personal_expected_rewards = (rewards / (weight + 1e-6))[strategy]
+        personal_expected_rewards = (rewards / (weight + DENOMINATOR_DELTA))[strategy]
         personal_expected_rewards = personal_expected_rewards * weight_choices
 
         weight = np.tile(weight, N).reshape(N, -1)
         for i, choice in enumerate(strategy):
             weight[i][choice] -= weights[i][choice]
 
-        weight_choices = np.tile(weight_choices.reshape(N, 1), [1, K])
+        weight_choices = weights
         weight = weight + weight_choices
         reward_deviation = (
-            np.tile(rewards.reshape(-1, K), [N, 1]) / (weight + 1e-6) * weight_choices
+            np.tile(rewards.reshape(-1, K), [N, 1]) / (weight + DENOMINATOR_DELTA) * weight_choices
         )
 
         for i, choice in enumerate(strategy):
-            reward_deviation[i][choice] = -1e6
+            reward_deviation[i][choice] = -THRESHOLD
 
         reward_best_deviation = np.max(reward_deviation, axis=1)
         delta_rewards = reward_best_deviation - personal_expected_rewards
-        if np.max(delta_rewards) > 1e-6:
+        if np.max(delta_rewards) > THRESHOLD:
             count += 1
 
     ans = count / N
@@ -277,12 +280,12 @@ if __name__ == "__main__":
     #         weights, rewards
     #     )
 
-    #     l = [ele[1] for ele in no_pne if ele[0] >= best_total_payoff - 1e-5]
+    #     l = [ele[1] for ele in no_pne if ele[0] >= best_total_payoff - THRESHOLD]
     #     delta_nopne = min(l) if len(l) > 0 else 100
 
     #     print(best_pne, best_total_payoff, best_delta_pne, delta_nopne)
 
-    #     # if best_delta_pne > delta_nopne + 1e-3:
+    #     # if best_delta_pne > delta_nopne + THRESHOLD:
     #     #     break
 
     #     if multiple_pne:

utils/utils.py
@@ -7,6 +7,8 @@ import numpy as np
 from joblib import Parallel, delayed
 from tqdm import tqdm
 
+THRESHOLD = 1e-4
+DENOMINATOR_DELTA = 1e-6
 
 def set_seed(seed):
     random.seed(seed)
@@ -22,9 +24,9 @@ def best_response_dynamics(N, K, mu, weights, threshold=1000):
         for i in range(N):
             k = res[i]
             w[k] -= weights[i][k]
-            r = mu * weights[i] / (w + weights[i] + 1e-6)
+            r = mu * weights[i] / (w + weights[i] + DENOMINATOR_DELTA)
             t = np.argmax(r)
-            if np.abs(r[t] - r[k]) < 1e-5:
+            if np.abs(r[t] - r[k]) < THRESHOLD:
                 w[k] += weights[i][k]
             else:
                 res[i] = t
@@ -59,9 +61,9 @@ def best_best_response_dynamics(N, K, mu, weights, threshold=1000, res=None):
         for i in range(N):
             k = res[i]
             w[k] -= weights[i][k]
-            r = mu * weights[i] / (w + weights[i] + 1e-9)
+            r = mu * weights[i] / (w + weights[i] + DENOMINATOR_DELTA)
             t = np.argmax(r)
-            if np.abs(r[t] - r[k]) >= 1e-5 and r[t] - r[k] > max_inc:
+            if np.abs(r[t] - r[k]) >= THRESHOLD and r[t] - r[k] > max_inc:
                 max_inc = r[t] - r[k]
                 ii = i
                 kk = k
@@ -73,13 +75,13 @@ def best_best_response_dynamics(N, K, mu, weights, threshold=1000, res=None):
             for i in range(N):
                 k = res[i]
                 reward.append(
-                    mu[k] * weights[i][k] / (np.sum(weights[:, k] * (res == k)) + 1e-9)
+                    mu[k] * weights[i][k] / (np.sum(weights[:, k] * (res == k)) + DENOMINATOR_DELTA)
                 )
             # print(N, K, res + 1, reward)
             return True
         else:
             w[kk] -= weights[ii][kk]
-            r = mu * weights[ii] / (w + weights[ii] + 1e-9)
+            r = mu * weights[ii] / (w + weights[ii] + DENOMINATOR_DELTA)
             w[tt] += weights[ii][tt]
             res[ii] = tt
             # print(res, ii, kk, tt, max_inc)